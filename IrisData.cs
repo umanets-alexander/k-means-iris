d6ef4c072f1169090e9e8fc232d125d3bd999824
Alexandr <umanets_alexander@mail.ru>
2021-03-30 10:02:32 +0700

    Первый релиз

---
+++ IrisData.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.ML.Data;
+
+namespace Knn_Iris
+{
+    public class IrisData
+    {
+        [LoadColumn(0)]
+        public float SepalLength;
+
+        [LoadColumn(1)]
+        public float SepalWidth;
+
+        [LoadColumn(2)]
+        public float PetalLength;
+
+        [LoadColumn(3)]
+        public float PetalWidth;
+
+        [LoadColumn(4)]
+        public string TypeIris;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
